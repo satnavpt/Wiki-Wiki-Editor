5e28b11ec2a245f8fb1422d8108126018d73a9f7
Zaidek <ethanlzajdek@gmail.com>
2022-02-24 21:28:41 +0000

    Split Preprocessing into two functions

--- Hate_Speech_Stage/preprocessing.py
+++ Hate_Speech_Stage/preprocessing.py
@@ -4,8 +4,8 @@ nltk.download('stopwords')
 from nltk.corpus import stopwords as stop
 
 
-# preprocess input text
-def preprocess(text):
+# preprocess training text
+def preprocess_training(text):
     
     # set all characters to lower case
     text = text.lower()
@@ -30,3 +30,19 @@ def preprocess(text):
     text = re.sub('\s+', ' ', text)
 
     return text
+
+
+# split input into sentences (needs work)
+def sentence_split(sample):
+    sentences = []
+    text = sample[1]
+    if len(text) > 100:
+        for i in range(0, len(text), 100):
+            sentences.append(text[i:i+100])
+    return sentences
+
+# preprocess prediction text
+def preprocess_prediction(text):
+    sentences = sentence_split(text)
+    sentences = [preprocess_training(sentence) for sentence in sentences]
+    return sentences
